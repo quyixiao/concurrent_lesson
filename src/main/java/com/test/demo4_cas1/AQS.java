3f80e41dc7125f879e32ee4bc1813a58f9657140
quyixiao <2621048238@qq.com>
2020-03-21 12:19:35 +0800

    提交修改

---
+++ src/main/java/com/test/demo4_cas1/AQS.java
@@ -0,0 +1,14 @@
+package com.test.demo4_cas1;
+
+public class AQS {
+    // AbstractQueue 队列中的QueueedSychronizer 同步发生器，构建 lock ,jUC ReentrantLock
+    // java.util.concurrent
+    // 基本思想
+    // 通过内置的FIFO 同步队列来完成线程的争夺资源的管理工作
+    // CLH 同步队列
+    //
+
+}
+
+
+
---
+++ src/main/java/com/test/demo5_aqs/AQSDemo01.java
@@ -0,0 +1,7 @@
+package com.test.demo5_aqs;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+public class AQSDemo01 {
+    AbstractQueuedSynchronizer aqs;
+}
