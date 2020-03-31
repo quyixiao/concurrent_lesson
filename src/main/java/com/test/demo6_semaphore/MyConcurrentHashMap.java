2d32ddda3f9011e50f20534bcc9849cd4c4788d6
quyixiao <2621048238@qq.com>
2020-03-31 15:00:23 +0800

    提交修改

--- src/main/java/com/test/demo6_semaphore/MyConcurrentHashMap.java
+++ src/main/java/com/test/demo6_semaphore/MyConcurrentHashMap.java
@@ -1596,52 +1596,85 @@ public class MyConcurrentHashMap<K, V> extends AbstractMap<K, V> implements Conc
     /**
      * Moves and/or copies the nodes in each bin to new table. See
      * above for explanation.
-     * https://www.cnblogs.com/softidea/p/10261414.html
+     * 1.计算每个线程可以处理的桶区间。默认 16.
+     * 2.初始化临时变量 nextTable，扩容 2 倍。
+     * 3.死循环，计算下标。完成总体判断。
+     * 4.如果桶内有数据，同步转移数据。通常会像链表拆成 2 份。
+     *我们假设长度是 32
+     * transfer 方法可以说很牛逼，很精华，内部多线程扩容性能很高，
+     *
+     * 通过给每个线程分配桶区间，避免线程间的争用，通过为每个桶节点加锁，避免 putVal 方法导致数据不一致。同时，在扩容的时候，也会将链表拆成两份，这点和 HashMap 的 resize 方法类似。
+     *
+     * 而如果有新的线程想 put 数据时，也会帮助其扩容。鬼斧神工，令人赞叹。
+     *
      */
     private final void transfer(MyConcurrentHashMap.Node<K, V>[] tab, MyConcurrentHashMap.Node<K, V>[] nextTab) {
         int n = tab.length, stride;
         // 1.通过计算 CPU 核心数和 Map 数组的长度得到每个线程（CPU）要帮助处理多少个桶，并且这里每个线程处理都是平均的。默认每个线程处理 16 个桶。因此，如果长度是 16 的时候，扩容的时候只会有一个线程扩容。
+        // 将 length / 8 然后除以 CPU核心数。如果得到的结果小于 16，那么就使用 16。
+        // 这里的目的是让每个 CPU 处理的桶一样多，避免出现转移任务不均匀的现象，如果桶较少的话，默认一个 CPU（一个线程）处理 16 个桶
         if ((stride = (NCPU > 1) ? (n >>> 3) / NCPU : n) < MIN_TRANSFER_STRIDE)
-            stride = MIN_TRANSFER_STRIDE; // subdivide range
+            stride = MIN_TRANSFER_STRIDE; // subdivide range  细分范围 stridea：TODO
         // 2.初始化临时变量 nextTable。将其在原有基础上扩容两倍。
         if (nextTab == null) {            // initiating
             try {
                 @SuppressWarnings("unchecked")
+                // 扩容  2 倍
                 MyConcurrentHashMap.Node<K, V>[] nt = (MyConcurrentHashMap.Node<K, V>[]) new MyConcurrentHashMap.Node<?, ?>[n << 1];
+                // 更新
                 nextTab = nt;
             } catch (Throwable ex) {      // try to cope with OOME
+                // 扩容失败， sizeCtl 使用 int 最大值。
                 sizeCtl = Integer.MAX_VALUE;
-                return;
+                return; // 结束
             }
+            // 更新成员变量
             nextTable = nextTab;
+            // 更新转移下标，就是 老的 tab 的 length
             transferIndex = n;
         }
-
+        // 新 tab 的 length
         int nextn = nextTab.length;
+        // 创建一个 fwd 节点，用于占位。当别的线程发现这个槽位中是 fwd 类型的节点，则跳过这个节点。
         MyConcurrentHashMap.ForwardingNode<K, V> fwd = new MyConcurrentHashMap.ForwardingNode<K, V>(nextTab);
+        // 首次推进为 true，如果等于 true，说明需要再次推进一个下标（i--），反之，如果是 false，那么就不能推进下标，需要将当前的下标处理完毕才能继续推进
         boolean advance = true;
+        // 完成状态，如果是 true，就结束此方法。
         boolean finishing = false; // to ensure sweep before committing nextTab
         // 3.死循环开始转移。多线程并发转移就是在这个死循环中，根据一个 finishing 变量来判断，该变量为 true 表示扩容结束，否则继续扩容。
         for (int i = 0, bound = 0; ; ) {
+
             MyConcurrentHashMap.Node<K, V> f;
             int fh;
             // 3.1 进入一个 while 循环，分配数组中一个桶的区间给线程，默认是 16. 从大到小进行分配。当拿到分配值后，进行 i-- 递减。
             // 这个 i 就是数组下标。（其中有一个 bound 参数，这个参数指的是该线程此次可以处理的区间的最小下标，超过这个下标，
             // 就需要重新领取区间或者结束扩容，还有一个 advance 参数，该参数指的是是否继续递减转移下一个桶，如果为 true，
             // 表示可以继续向后推进，反之，说明还没有处理好当前桶，不能推进)
+            // 如果当前线程可以向后推进；这个循环就是控制 i 递减。同时，每个线程都会进入这里取得自己需要转移的桶的区间
             while (advance) {
                 int nextIndex, nextBound;
+                // 对 i 减一，判断是否大于等于 bound （正常情况下，如果大于 bound 不成立，说明该线程上次领取的任务已经完成了。那么，需要在下面继续领取任务）
+                // 如果对 i 减一大于等于 bound（还需要继续做任务），或者完成了，修改推进状态为 false，不能推进了。任务成功后修改推进状态为 true。
+                // 通常，第一次进入循环，i-- 这个判断会无法通过，从而走下面的 nextIndex 赋值操作（获取最新的转移下标）。其余情况都是：如果可以推进，将 i 减一，然后修改成不可推进。如果 i 对应的桶处理成功了，改成可以推进。
                 if (--i >= bound || finishing)
-                    advance = false;
+                    advance = false; // 这里设置 false，是为了防止在没有成功处理一个桶的情况下却进行了推进
+                // 这里的目的是：1. 当一个线程进入时，会选取最新的转移下标。2. 当一个线程处理完自己的区间时，如果还有剩余区间的没有别的线程处理。再次获取区间。
                 else if ((nextIndex = transferIndex) <= 0) {
+                    // 如果小于等于0，说明没有区间了 ，i 改成 -1，推进状态变成 false，不再推进，表示，扩容结束了，当前线程可以退出了
+                    // 这个 -1 会在下面的 if 块里判断，从而进入完成状态判断
                     i = -1;
+                    // 这里设置 false，是为了防止在没有成功处理一个桶的情况下却进行了推进
                     advance = false;
-                } else if (U.compareAndSwapInt
+                } // CAS 修改 transferIndex，即 length - 区间值，留下剩余的区间值供后面的线程使用
+                else if (U.compareAndSwapInt
                         (this, TRANSFERINDEX, nextIndex,
                                 nextBound = (nextIndex > stride ?
                                         nextIndex - stride : 0))) {
+                    // 这个值就是当前线程可以处理的最小当前区间最小下标
                     bound = nextBound;
+                    // 初次对i 赋值，这个就是当前线程可以处理的当前区间的最大下标
                     i = nextIndex - 1;
+                    // 这里设置 false，是为了防止在没有成功处理一个桶的情况下却进行了推进，这样对导致漏掉某个桶。下面的 if (tabAt(tab, i) == f) 判断会出现这样的情况。
                     advance = false;
                 }
             }
@@ -1649,67 +1682,100 @@ public class MyConcurrentHashMap<K, V> extends AbstractMap<K, V> implements Conc
             // 如果没完成，但已经无法领取区间（没了），该线程退出该方法，并将 sizeCtl 减一，表示扩容的线程少一个了。如果减完这
             // 个数以后，sizeCtl 回归了初始状态，表示没有线程再扩容了，该方法所有的线程扩容结束了。（这里主要是判断扩容任务是否结束，
             // 如果结束了就让线程退出该方法，并更新相关变量）。然后检查所有的桶，防止遗漏。
+            //3.3 如果没有完成任务，且 i 对应的槽位是空，尝试 CAS 插入占位符，让 putVal 方法的线程感知。
+            //3.4 如果 i 对应的槽位不是空，且有了占位符，那么该线程跳过这个槽位，处理下一个槽位。
+            //3.5 如果以上都是不是，说明这个槽位有一个实际的值。开始同步处理这个桶。
+            //3.6 到这里，都还没有对桶内数据进行转移，只是计算了下标和处理区间，然后一些完成状态判断。同时，如果对应下标内没有数据或已经被占位了，就跳过了。
+            // 如果 i 小于0 （不在 tab 下标内，按照上面的判断，领取最后一段区间的线程扩容结束）
+            //  如果 i >= tab.length(不知道为什么这么判断)
+            //  如果 i + tab.length >= nextTable.length  （不知道为什么这么判断）
             if (i < 0 || i >= n || i + n >= nextn) {
                 int sc;
+                // 如果完成了扩容
                 if (finishing) {
+                    // 删除成员变量
                     nextTable = null;
+                    // 更新 table
                     table = nextTab;
+                    // 更新阈值
                     sizeCtl = (n << 1) - (n >>> 1);
-                    return;
+                    return;// 结束方法。
                 }
+                // 如果没完成
+                // 尝试将 sc -1. 表示这个线程结束帮助扩容了，将 sc 的低 16 位减一。
                 if (U.compareAndSwapInt(this, SIZECTL, sc = sizeCtl, sc - 1)) {
-                    if ((sc - 2) != resizeStamp(n) << RESIZE_STAMP_SHIFT)
-                        return;
-                    finishing = advance = true;
-                    i = n; // recheck before commit
+                    if ((sc - 2) != resizeStamp(n) << RESIZE_STAMP_SHIFT)// 如果 sc - 2 不等于标识符左移 16 位。如果他们相等了，说明没有线程在帮助他们扩容了。也就是说，扩容结束了。
+                        return; // 不相等，说明没结束，当前线程结束方法。
+                    finishing = advance = true; // 如果相等，扩容结束了，更新 finising 变量
+                    i = n; // recheck before commit // 再次循环检查一下整张表
                 }
-            } else if ((f = tabAt(tab, i)) == null)
-                advance = casTabAt(tab, i, null, fwd);
-            else if ((fh = f.hash) == MOVED)
-                advance = true; // already processed
-            else {
+            } else if ((f = tabAt(tab, i)) == null)// 获取老 tab i 下标位置的变量，如果是 null，就使用 fwd 占位。
+                advance = casTabAt(tab, i, null, fwd); // 如果成功写入 fwd 占位，再次推进一个下标
+            else if ((fh = f.hash) == MOVED) // 如果不是 null 且 hash 值是 MOVED。
+                advance = true;// already processed // 说明别的线程已经处理过了，再次推进一个下标
+            else {// 到这里，说明这个位置有实际值了，且不是占位符。对这个节点上锁。为什么上锁，防止 putVal 的时候向链表插入数据
+                // 4.处理每个桶的行为都是同步的。防止 putVal 的时候向链表插入数据。
                 synchronized (f) {
-                    if (tabAt(tab, i) == f) {
+                    // 判断 i 下标处的桶节点是否和 f 相同
+                    if (tabAt(tab, i) == f) { // low, height 高位桶，低位桶
                         MyConcurrentHashMap.Node<K, V> ln, hn;
+                        // 4.1 如果这个桶是链表，那么就将这个链表根据 length 取于拆成两份，取于结果是 0 的放在新表的低位，取于结果是 1 放在新表的高位。
+                        // 如果 f 的 hash 值大于 0 。TreeBin 的 hash 是 -2
                         if (fh >= 0) {
+                            // 对老长度进行与运算（第一个操作数的的第n位于第二个操作数的第n位如果都是1，那么结果的第n为也为1，否则为0）
+                            // 由于 Map 的长度都是 2 的次方（000001000 这类的数字），那么取于 length 只有 2 种结果，一种是 0，一种是1
+                            //  如果是结果是0 ，Doug Lea 将其放在低位，反之放在高位，目的是将链表重新 hash，放到对应的位置上，让新的取于算法能够击中他。
                             int runBit = fh & n;
-                            MyConcurrentHashMap.Node<K, V> lastRun = f;
+                            MyConcurrentHashMap.Node<K, V> lastRun = f;// 尾节点，且和头节点的 hash 值取于不相等
+                            // 遍历这个桶
                             for (MyConcurrentHashMap.Node<K, V> p = f.next; p != null; p = p.next) {
+                                // 取于桶中每个节点的 hash 值
                                 int b = p.hash & n;
+                                // 如果节点的 hash 值和首节点的 hash 值取于结果不同
                                 if (b != runBit) {
-                                    runBit = b;
-                                    lastRun = p;
+                                    runBit = b; // 更新 runBit，用于下面判断 lastRun 该赋值给 ln 还是 hn。
+                                    lastRun = p; // 这个 lastRun 保证后面的节点与自己的取于值相同，避免后面没有必要的循环
                                 }
                             }
-                            if (runBit == 0) {
+                            if (runBit == 0) {// 如果最后更新的 runBit 是 0 ，设置低位节点
                                 ln = lastRun;
                                 hn = null;
                             } else {
-                                hn = lastRun;
+                                hn = lastRun; // 如果最后更新的 runBit 是 1， 设置高位节点
                                 ln = null;
                             }
+                            // 再次循环，生成两个链表，lastRun 作为停止条件，这样就是避免无谓的循环（lastRun 后面都是相同的取于结果）
                             for (MyConcurrentHashMap.Node<K, V> p = f; p != lastRun; p = p.next) {
                                 int ph = p.hash;
                                 K pk = p.key;
                                 V pv = p.val;
-                                if ((ph & n) == 0)
+                                // 如果与运算结果是 0，那么就还在低位
+                                if ((ph & n) == 0)  // 如果是0 ，那么创建低位节点
                                     ln = new MyConcurrentHashMap.Node<K, V>(ph, pk, pv, ln);
-                                else
+                                else  // 1 则创建高位
                                     hn = new MyConcurrentHashMap.Node<K, V>(ph, pk, pv, hn);
                             }
+                            // 其实这里类似 hashMap
+                            // 设置低位链表放在新链表的 i
                             setTabAt(nextTab, i, ln);
+                            // 设置高位链表，在原有长度上加 n
                             setTabAt(nextTab, i + n, hn);
+                            // 将旧的链表设置成占位符
                             setTabAt(tab, i, fwd);
+                            // 继续向后推进
                             advance = true;
+                        //4.2 如果这个桶是红黑数，那么也拆成 2 份，方式和链表的方式一样，然后，判断拆分过的树的节点数量，如果数量小于等于 6，改造成链表。反之，继续使用红黑树结构。
                         } else if (f instanceof MyConcurrentHashMap.TreeBin) {
                             MyConcurrentHashMap.TreeBin<K, V> t = (MyConcurrentHashMap.TreeBin<K, V>) f;
                             MyConcurrentHashMap.TreeNode<K, V> lo = null, loTail = null;
                             MyConcurrentHashMap.TreeNode<K, V> hi = null, hiTail = null;
                             int lc = 0, hc = 0;
+                            // 遍历
                             for (MyConcurrentHashMap.Node<K, V> e = t.first; e != null; e = e.next) {
                                 int h = e.hash;
                                 MyConcurrentHashMap.TreeNode<K, V> p = new MyConcurrentHashMap.TreeNode<K, V>
                                         (h, e.key, e.val, null, null);
+                                // 和链表相同的判断，与运算 == 0 的放在低位
                                 if ((h & n) == 0) {
                                     if ((p.prev = loTail) == null)
                                         lo = p;
@@ -1717,7 +1783,7 @@ public class MyConcurrentHashMap<K, V> extends AbstractMap<K, V> implements Conc
                                         loTail.next = p;
                                     loTail = p;
                                     ++lc;
-                                } else {
+                                } else {   // 不是 0 的放在高位
                                     if ((p.prev = hiTail) == null)
                                         hi = p;
                                     else
@@ -1726,13 +1792,18 @@ public class MyConcurrentHashMap<K, V> extends AbstractMap<K, V> implements Conc
                                     ++hc;
                                 }
                             }
+                            // 如果树的节点数小于等于 6，那么转成链表，反之，创建一个新的树
                             ln = (lc <= UNTREEIFY_THRESHOLD) ? untreeify(lo) :
                                     (hc != 0) ? new MyConcurrentHashMap.TreeBin<K, V>(lo) : t;
                             hn = (hc <= UNTREEIFY_THRESHOLD) ? untreeify(hi) :
                                     (lc != 0) ? new MyConcurrentHashMap.TreeBin<K, V>(hi) : t;
+                            // 低位树
                             setTabAt(nextTab, i, ln);
+                            // 高位数
                             setTabAt(nextTab, i + n, hn);
+                            // 旧的设置成占位符
                             setTabAt(tab, i, fwd);
+                            // 继续向后推进
                             advance = true;
                         }
                     }
